92c8ca3ac240a56bc1dd760e9cbcf0c4170e33b7
Essawey <M.Abdelmaged@nu.edu.eg>
2024-12-28 21:40:24 +0200

    edits

--- config.py
+++ config.py
@@ -1,18 +1,26 @@
-ClientID = -1
-num_cpus = 32
-num_gpus = 2
-train_batch_size = 4
-val_batch_size = 4
-test_batch_size = 4
-validation_split_per_client = 0.1
-test_split = 0.1
-num_clients = 4
-epochs = 1
-num_rounds = 3
-learning_rate = 0.01
-validation_freq = 1
-early_stopping_patience = 10
-DiceWeight = 1/3
-TverskyWeight = 1/3
-IoUWeight = 1/3
-CFCWeight = 0/4
+DEVICE = 'cpu'
+EPOCHS = 2
+LR = 0.003
+BATCH_SIZE = 8
+IMG_SIZE = 512
+ENCODER = 'timm-efficientnet-b0'
+WEIGHTS = 'imagenet'
+
+
+
+# ClientID = -1
+# num_cpus = 32
+# num_gpus = 2
+# train_batch_size = 4
+# val_batch_size = 4
+# test_batch_size = 4
+# num_clients = 4
+# epochs = 1
+# num_rounds = 3
+# learning_rate = 0.01
+# validation_freq = 1
+# early_stopping_patience = 10
+# DiceWeight = 1/3
+# TverskyWeight = 1/3
+# IoUWeight = 1/3
+# CFCWeight = 0/4
