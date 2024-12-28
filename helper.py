29f79d9f68e274b53d2c75f06b1063f9d734fc5a
Essawey <M.Abdelmaged@nu.edu.eg>
2024-12-28 21:32:59 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -16,3 +16,29 @@ def binarizeChannel(masks):
             masks_binary[mask_idx, channel_idx] = current_channel
 
     return masks_binary
+
+
+def plot(trainMasks, idx):
+    fig, axes = plt.subplots(2, 3, figsize=(10, 6))
+
+    channel_images = []
+
+    for channel_index in range(trainMasks.shape[3]):
+
+        max_value = np.max(trainMasks[idx, :, :, channel_index])
+
+        colors = plt.cm.get_cmap('tab20', int(max_value + 1))
+
+        row = channel_index // 3
+        col = channel_index % 3
+
+        ax = axes[row, col]
+        im = ax.imshow(trainMasks[idx, :, :, channel_index], cmap=colors, vmin=0, vmax=max_value)
+        ax.set_title(f'Channel {channel_index} : {list(labels_idx.values())[channel_index]}')
+        ax.axis('off')
+        channel_images.append(im)
+
+    cbar_ax = fig.add_axes([0.15, -0.02, 0.7, 0.03])
+    cbar = fig.colorbar(channel_images[0], cax=cbar_ax, orientation='horizontal')
+    plt.tight_layout()
+    plt.show()
