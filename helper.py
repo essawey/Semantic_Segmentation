6cb65c59b10fc39c3c6f8100c862fc1afbe6c6cc
Essawey <M.Abdelmaged@nu.edu.eg>
2024-12-29 14:18:13 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -61,3 +61,42 @@ def show_mask(mask):
     fig.colorbar(channel_images[0], cax=cbar_ax, orientation='horizontal')
     plt.tight_layout()
     plt.show()
+
+
+def create_patches(image_dir, patch_size, target_dir):
+    for path, _, _ in sorted(os.walk(image_dir)):
+        relative_path = os.path.relpath(path, image_dir)
+        target_path = Path(target_dir) / relative_path
+        target_path.mkdir(parents=True, exist_ok=True)
+
+        images_index, masks_index = 0, 0
+
+        images = sorted(os.listdir(path))
+        for image_name in images:
+            if image_name.endswith(".png") or image_name.endswith(".npy"):
+                if image_name.endswith(".npy"):
+                    image = np.load(os.path.join(path, image_name))
+                    image = np.transpose(image, (1, 2, 0)) # (h, w, c)
+                    image = np.argmax(image, axis=-1) # (h, w)
+                    image = image.astype(np.uint8)
+                else:
+                    image = cv2.imread(os.path.join(path, image_name))
+                size_X, size_Y = math.ceil(image.shape[1]/patch_size), math.ceil(image.shape[0]/patch_size)
+                pad_X, pad_Y = (patch_size * size_X - image.shape[1]) / (size_X - 1), (patch_size * size_Y - image.shape[0]) / (size_Y - 1)
+                image = Image.fromarray(image)
+                top = 0
+                for y in range(size_Y):
+                    left = 0
+                    for x in range(size_X):
+                        crop_image = transforms.functional.crop(image, top, left, patch_size, patch_size)
+                        crop_image = np.array(crop_image)
+                        if image_name.endswith('.png'):
+                            patch_name = f"{Path(image_name).stem}_patch{images_index}.png"
+                            cv2.imwrite(str(target_path / patch_name), crop_image)
+                            images_index += 1
+                        else:
+                            patch_name = f"{Path(image_name).stem}_patch{masks_index}.npy"
+                            np.save(str(target_path / patch_name), crop_image)
+                            masks_index += 1
+                        left = left + patch_size - pad_X
+                    top = top + patch_size - pad_Y
