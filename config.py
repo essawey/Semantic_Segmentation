eda52569b1e1ec897bbcbab0403213dba79d2b99
Essawey <M.Abdelmaged@nu.edu.eg>
2024-12-30 16:18:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,17 +7,11 @@ ENCODER = 'efficientnet-b4'
 ENCODER_WEIGHTS = 'imagenet'
 NUM_OF_CLASSES = 6
 
-
 # ClientID = -1
 # num_cpus = 32
 # num_gpus = 2
-# train_batch_size = 4
-# val_batch_size = 4
-# test_batch_size = 4
 # num_clients = 4
-# epochs = 1
 # num_rounds = 3
-# learning_rate = 0.01
 # validation_freq = 1
 # early_stopping_patience = 10
 # DiceWeight = 1/3
